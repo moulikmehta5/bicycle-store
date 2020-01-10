9b200b7fd56bf261de399bfb5e15c6ac8d7c7be3
Moulik <moulik.mehta@gmail.com>
2020-01-09 19:18:50 -0500

    Bicycle cart change

--- views/bicycle-cart.php
+++ views/bicycle-cart.php
@@ -1,44 +1,28 @@
 <?php
     require('../repository/formRepository.php');
 
-    session_start();
- 
-
+    session_start(); 
 
     $repository = new FormRepository();
     $result = $repository->getCurrentSelection(); 
+    
     while ($row = $result->fetch_assoc()) {
         $brand = $row['brand'];
         $gender = $row['gender'];
         $color = $row['color'];
     }
     
+    $price = $repository->getPriceFromDB($brand, $gender, $color);
+    $repository->saveCurrentPrice($price);
+    $allRows = $repository->getAllSelection($brand, $color, $gender);
    
-    
-    if (isset($_POST["btnSubmit"])) {
+    if (isset($_POST["radioSubmit"])) {
+        $newPrice = $_POST["radio"];
+        $repository->saveCurrentPrice($newPrice);
         header("location: billing.php");
     }
     
-    $price = $repository->getPriceFromDB($brand, $gender, $color);
-    $repository->saveCurrentPrice($price);
-     $allRows = $repository->getAllSelection($brand, $color, $gender);
-     // 
      
-     if(isset($_POST['radio'])) {
-
-        $brand = $_POST['name'];
-        $gender = $_POST['Type'];
-        $color = $_POST['color_type'];
-        $price = $_POST['price'];
-
-        
-            $repository = new FormRepository();
-            $repository->saveCurrentOption($brand,  $gender, $color, $price);
-            ;
-            
-                   
-    }
-    
 ?>
 
 <!DOCTYPE html>
@@ -53,50 +37,41 @@
     </div>
         
         <div id="welcome-page">
-            <div class="row jumbotron text-center">  
-     
+            <div class="row jumbotron text-center">
                 <p> Bicycle Cart </p> 
                 <p> Price as per your selection </p>
+                <form method="post">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col"> Option</th>
+                                <th scope="col"> Brand </th>
+                                <th scope="col"> Type </th>
+                                <th scope="col"> Color </th>
+                                <th scope="col"> Price </th>
 
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th scope="col"> Option</th>
-                            <th scope="col"> Brand </th>
-                            <th scope="col"> Type </th>
-                            <th scope="col"> Color </th>
-                            <th scope="col"> Price </th>
-
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php    
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php    
                                 if ($allRows->num_rows > 0) {
-                                   // output data of each row
-                               while($row = $allRows->fetch_assoc()) {
-                                 echo '<tr>
-                                         <td scope="row"><input name="radio" type="radio" value="Option"></td>
-                                         <td scope="row">' . $row["name"] .'</td>
-                                         <td> '.$row["Type"] .'</td>
-                                         <td> '.$row["color_type"] .'</td>
-                                         <td> '.$row["price"] .'</td>
-                                      </tr>';
-                    }
-                }
-                
-                 
-                ?>
-                    </tbody>    
-                </table>
-
-                <div> 
-                    <form method="post">
-                        <input type="submit" name="btnSubmit" class="btn btn-primary" />                       
-                    </form>               
-                </div>
-
+                                    
+                                    while($row = $allRows->fetch_assoc()) {
+                                        echo '<tr>
+                                            <td scope="row"><input name="radio" type="radio" value="' . $row["price"] . '"></td>
+                                            <td scope="row">' . $row["name"] .'</td>
+                                            <td> '.$row["Type"] .'</td>
+                                            <td> '.$row["color_type"] .'</td>
+                                            <td> '.$row["price"] .'</td>
+                                        </tr>';
+                                    }
+                                }
+                            ?>
+                        </tbody>    
+                    </table>                    
+                    <input type="submit" name="radioSubmit" class="btn btn-primary" />                       
+                </form>              
             </div>
-
         </div>
       </body>
 </html>
--- views/order-details.php
+++ views/order-details.php
@@ -4,15 +4,13 @@
     
     session_start();
  
-    // Check if the user is logged in, if not then redirect him to login page
-    // 
     if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         header("location: billing.php");
         exit;
     }
 
     $repository = new FormRepository();
-    $result = $repository->getCurrentOption();
+    $result = $repository->getCurrentSelection();
     
     while ($row = $result->fetch_assoc()) {
         $brand = $row['brand'];
